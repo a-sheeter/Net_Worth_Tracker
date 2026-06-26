412b43943ea3f2b3f8a44c44ccff2ae609f2943d
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-06-26 15:45:54 -0400

    add account functionality

--- src/pages/AccountForm.jsx
+++ src/pages/AccountForm.jsx
@@ -1,6 +1,14 @@
+// react
 import { useState, useEffect } from "react";
 
+// hooks
+import useUser from "../hooks/useUser";
+
+// constants
 import { accountTypes } from "../constants/accountTypes";
+
+// utils
+import { supabase } from "../utils/supabase";
 
 export default function AccountForm() {
     /* --- Effect --- */
@@ -8,9 +16,121 @@ export default function AccountForm() {
         document.title = "Add Account | Net Worth Tracker";
     }, []);
 
+    const { user } = useUser();
+
+    /* --- State --- */
+    const [name, setName] = useState("");
+    const [typeId, setTypeId] = useState("");
+    const [subtypeId, setSubtypeId] = useState("");
+    const [url, setUrl] = useState("");
+    const [balance, setBalance] = useState("");
+
+    const selectedType = accountTypes.find(
+        type => type.id === typeId
+    );
+
+    const balanceType = selectedType?.balanceType;
+
+    /* --- Errors --- */
+    const [errorMessage, setErrorMessage] = useState("");
+
+    /* --- Form --- */
+    function resetForm() {
+        setName("");
+        setTypeId("");
+        setSubtypeId("");
+        setUrl("");
+        setBalance("");
+    }
+
+    /* --- Handlers --- */
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        setErrorMessage("");
+
+        const { error } = await supabase
+            .from("accounts")
+            .insert([
+                {
+                    user_id: user.id,
+                    name: name, 
+                    account_type: typeId,
+                    account_subtype: subtypeId,
+                    url: url,
+                    balance: balance,
+                    balance_type: balanceType
+                }
+            ])
+
+        if (error) {
+            setErrorMessage(error.message);
+            return;
+        }
+
+        resetForm();
+    }
+
     return (
         <>
-            <p>Hello</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input
+                    id="name"
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <label htmlFor="account-type">Account Type</label>
+                <select id="account-type" name="account-type" onChange={(e) => {
+                    setTypeId(e.target.value); setSubtypeId(""); 
+                }}>
+                    <option value="">Choose Account Type</option>
+                    {accountTypes.map(type => (
+                        <option
+                            key={type.id}
+                            value={type.id}
+                        >
+                            {type.label}
+                        </option>
+                    ))
+                    }
+                </select>
+
+                <label htmlFor="account-subtype">Account Sub-Type</label>
+                <select id="account-subtype" name="account-subtype" onChange={(e) => setSubtypeId(e.target.value)}>
+                    <option value="">Choose Account Subtype</option>
+                    {selectedType?.subtypes.map(subtype => (
+                        <option
+                            key={subtype.id}
+                            value={subtype.id}
+                        >
+                            {subtype.label}
+                        </option>
+                    ))}
+                </select>
+
+                <label htmlFor="url">Url to login</label>
+                <input
+                    id="url"
+                    type="text"
+                    required
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                />
+
+                <label htmlFor="balance">Balance</label>
+                <input
+                    id="balance"
+                    type="number"
+                    required
+                    value={balance}
+                    onChange={(e) => setBalance(e.target.value)}
+                />
+
+                <button type="submit">Add Account</button>
+            </form>
         </>
     )
 }
