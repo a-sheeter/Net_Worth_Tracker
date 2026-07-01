988ffc113f18de334ba5f90b9be3b9c1a9d6144b
Amy Sheeter <90341586+a-sheeter@users.noreply.github.com>
2026-07-01 12:35:09 -0400

    integrate asset and liability table components

---
+++ src/components/LiabilityTable.jsx
@@ -0,0 +1,69 @@
+// react
+import { Link } from "react-router-dom";
+
+// hooks
+import useAccounts from "../hooks/useAccounts";
+
+// data
+import { accountTypes } from "../constants/accountTypes";
+
+export default function LiabilityTable() {
+
+    /* --- State --- */
+    const {
+        accounts,
+        handleDeleteAccount
+    } = useAccounts();
+
+    return (
+        <>
+            <h2>Liabilities</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Balance</td>
+                        <td>Account Type</td>
+                        <td>Account Subtype</td>
+                        <td>Last Updated</td>
+                        <td>Visit Account</td>
+                        <td>Actions</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {accounts.map(account => {
+                        const lastUpdated = new Date(account.last_updated);
+
+                        const selectedType = accountTypes.find(
+                            type => type.id === account.account_type
+                        );
+
+                        const selectedSubtype = selectedType.subtypes.find(
+                            subtype => subtype.id === account.account_subtype
+                        );
+
+                        const formatter = new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                            currencySign: 'accounting'
+                        });
+
+                        if (account.balance_type === "liability") {
+                            return (
+                                <tr key={account.id}>
+                                    <td>{account.name}</td>
+                                    <td>{formatter.format(account.balance)}</td>
+                                    <td>{selectedType?.label ?? account.account_type}</td>
+                                    <td>{selectedSubtype?.label ?? account.account_subtype}</td>
+                                    <td>{lastUpdated.toLocaleDateString()}{" "} {lastUpdated.toLocaleTimeString()}</td>
+                                    <td><Link target="_blank" to={account.url}>{account.name}</Link></td>
+                                    <td>Edit | <button type="button" onClick={() => handleDeleteAccount(account.id)}>Delete</button></td>
+                                </tr>
+                            )
+                        }
+                    })}
+                </tbody>
+            </table>
+        </>
+    )
+}
--- src/pages/accounts.jsx
+++ src/pages/accounts.jsx
@@ -1,9 +1,8 @@
 // react
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 // hooks
-import useUser from "../hooks/useUser";
 import useAccounts from "../hooks/useAccounts";
 
 // data
@@ -11,16 +10,11 @@ import { accountTypes } from "../constants/accountTypes";
 
 // components
 import NavBar from "../components/NavBar";
+import AssetTable from "../components/AssetTable";
+import LiabilityTable from "../components/LiabilityTable";
 
-// utils
-import { supabase } from "../utils/supabase";
-
-
-
+// Render
 export default function Accounts() {
-    /* --- consts --- */
-    const { user } = useUser();
-    const navigate = useNavigate();
 
     /* --- State --- */
     const {
@@ -36,101 +30,8 @@ export default function Accounts() {
     return (
         <>
             <NavBar />
-            <h2>Assets</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Balance</td>
-                        <td>Account Type</td>
-                        <td>Account Subtype</td>
-                        <td>Last Updated</td>
-                        <td>Visit Account</td>
-                        <td>Actions</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {accounts.map(account => {
-                        const lastUpdated = new Date(account.last_updated);
-
-                        const selectedType = accountTypes.find(
-                            type => type.id === account.account_type
-                        );
-
-                        const selectedSubtype = selectedType.subtypes.find(
-                            subtype => subtype.id === account.account_subtype
-                        );
-
-                        const formatter = new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD',
-                            currencySign: 'accounting'
-                        });
-
-                        if (account.balance_type === "asset") {
-                            return (
-                                <tr key={account.id}>
-                                    <td>{account.name}</td>
-                                    <td>{formatter.format(account.balance)}</td>
-                                    <td>{selectedType?.label ?? account.account_type}</td>
-                                    <td>{selectedSubtype?.label ?? account.account_subtype}</td>
-                                    <td>{lastUpdated.toLocaleDateString()}{" "} {lastUpdated.toLocaleTimeString()}</td>
-                                    <td><Link target="_blank" to={account.url}>{account.name}</Link></td>
-                                    <td>Edit | <button type="button" onClick={() => handleDeleteAccount(account.id)}>Delete</button></td>
-                                </tr>
-                            )
-                        }
-                    })}
-                </tbody>
-            </table>
-
-            <h2>Liabilities</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Balance</td>
-                        <td>Account Type</td>
-                        <td>Account Subtype</td>
-                        <td>Last Updated</td>
-                        <td>Visit Account</td>
-                        <td>Actions</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {accounts.map(account => {
-                        const lastUpdated = new Date(account.last_updated);
-
-                        const selectedType = accountTypes.find(
-                            type => type.id === account.account_type
-                        );
-
-                        const selectedSubtype = selectedType.subtypes.find(
-                            subtype => subtype.id === account.account_subtype
-                        );
-
-                        const formatter = new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD',
-                            currencySign: 'accounting'
-                        });
-
-                        if (account.balance_type === "liability") {
-                            return (
-                                <tr key={account.id}>
-                                    <td>{account.name}</td>
-                                    <td>{formatter.format(account.balance)}</td>
-                                    <td>{selectedType?.label ?? account.account_type}</td>
-                                    <td>{selectedSubtype?.label ?? account.account_subtype}</td>
-                                    <td>{lastUpdated.toLocaleDateString()}{" "} {lastUpdated.toLocaleTimeString()}</td>
-                                    <td><Link target="_blank" to={account.url}>{account.name}</Link></td>
-                                    <td>Edit | <button type="button" onClick={() => handleDeleteAccount(account.id)}>Delete</button></td>
-                                </tr>
-                            )
-                        }
-                    })}
-                </tbody>
-            </table>
+            <AssetTable/>
+            <LiabilityTable/>
         </>
     )
 }
